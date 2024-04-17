35ee46662c4f0ba16a2581b43b1997b5f513b4c2
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-17 22:20:17 +0200

    Added inventoryItem Class

---
+++ InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_ICE_Task__2
+{
+    internal class InventoryItem
+    {
+        public string? ItemName { get; set; }
+        public double ItemPrice { get; set; }
+        public int ItemQuantity { get; set; }
+        public string? ItemCategory { get; set; }
+
+    }
+}
