6d9606298f73653a52df277f09cf05057bc67fb4
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-17 22:40:41 +0200

    Added enum for categories of items

---
+++ Categories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_ICE_Task__2
+{
+    public enum ItemCategory
+    {
+        Produce,
+        Dairy,
+        Meat,
+        Seafood,
+        Bakery,
+        Frozen_Foods,
+        Canned_Food,
+        Beverages,
+        Snacks,
+        Kitchen_Essentials,
+        Condiments,
+        Cleaning_Supplies,
+        Personal_Care,
+        Health_and_Wellness,
+        Other
+    }
+}
---
+++ Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_ICE_Task__2
+{
+    internal class Inventory
+    {
+        //key = categories
+        //value = list of items in the category
+        Dictionary<string, List<InventoryItem>> inventory = new Dictionary<string, List<InventoryItem>>();
+
+        //Keep track of history of added items
+        //item added pushed onto stack
+        //item removed popped from stack
+        //LIFO
+        Stack<InventoryItem> AddItemHistory = new Stack<InventoryItem>();
+
+        //keep track of items that need to be restocked
+        //quantity goes below certain amount, added to queue to order
+        //FIFO
+        Queue<InventoryItem> queue = new Queue<InventoryItem>();
+
+
+    }
+}
