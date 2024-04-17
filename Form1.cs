22f0995c561a087bc5276736544708f123ec71ef
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-17 22:15:52 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace ST10361554_PROG6221_ICE_Task__2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
