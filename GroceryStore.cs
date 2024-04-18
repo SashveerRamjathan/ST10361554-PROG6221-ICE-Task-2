6b811a3b78c58a04f99dfac6299b16d5d9407bd0
SashveerRamjathan <sashveerlakhan0@gmail.com>
2024-04-18 23:51:04 +0200

    Added grocery store class

---
+++ GroceryStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10361554_PROG6221_ICE_Task__2
+{
+    internal class GroceryStore
+    {
+        public Inventory? inventory { get; set; }
+    }
+}
